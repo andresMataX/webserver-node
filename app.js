aa02843d83e68354873dc9ad731619ec4b74d676
Andrés Mata <andres_mata21@outlook.com>
2022-05-27 23:46:16 -0500

    Fix: Funciones tradicionales a de flecha

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Home Page');
 });
 
-app.get('/hola-mundo', function (req, res) {
+app.get('/hola-mundo', (req, res) => {
     res.send('Saludando al mundo');
 });
 
-app.get('*', function (req, res) {
+app.get('*', (req, res) => {
     res.send('404 | Page Not Found');
 });
 
