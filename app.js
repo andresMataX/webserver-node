6304121a42693d53d30f06cad20c4737a18cfc9f
Andrés Mata <andres_mata21@outlook.com>
2022-05-28 00:11:13 -0500

    Simplificación de rutas sin .html

--- app.js
+++ app.js
@@ -15,6 +15,15 @@ app.get('/hola-mundo', (req, res) => {
     res.send('Saludando al mundo');
 });
 
+// Ruta para quitar el .html de generic
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+});
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
 });
