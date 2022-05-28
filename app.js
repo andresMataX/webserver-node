36fbb2b396b90f40b7c59b86d554a05b403bea84
Andrés Mata <andres_mata21@outlook.com>
2022-05-27 23:37:30 -0500

    Descarga de un archivo csv en una ruta

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const http = require('http');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    res.writeHead(200, { 'Content-Type': 'application/csv' });
 
-    const persona = {
-        id: 1,
-        nombre: 'Andrés'
-    }
+    // const persona = {
+    //     id: 1,
+    //     nombre: 'Andrés'
+    // }
 
-    res.write(JSON.stringify(persona));
+    res.write('id, nombre\n');
+    res.write('1, Andres\n');
+    res.write('2, Fernando\n');
     res.end();
 
 }).listen(8080);
