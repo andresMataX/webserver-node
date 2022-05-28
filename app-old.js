48de53278faba5f8c524c211a2599aeffad7e01f
Andrés Mata <andres_mata21@outlook.com>
2022-05-27 23:40:01 -0500

    Renombre de app

--- app-old.js
+++ app-old.js
@@ -3,17 +3,19 @@ const http = require('http');
 http.createServer((req, res) => {
 
     // res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
-    res.writeHead(200, { 'Content-Type': 'application/csv' });
+    // res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    // res.writeHead(200, { 'Content-Type': 'application/csv' });
 
     // const persona = {
     //     id: 1,
     //     nombre: 'Andrés'
     // }
 
-    res.write('id, nombre\n');
-    res.write('1, Andres\n');
-    res.write('2, Fernando\n');
+    // res.write('id, nombre\n');
+    // res.write('1, Andres\n');
+    // res.write('2, Fernando\n');
+
+    res.write('Hola Mundo');
     res.end();
 
 }).listen(8080);
